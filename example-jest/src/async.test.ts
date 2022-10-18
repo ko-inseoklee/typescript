8a0c40f0326cd72308e46576c208a8a43ddf3f89
ko-inseoklee <tjrkd222@gmail.com>
2022-10-18 16:08:45 +0900

    delete regacy repo

--- example-jest/src/async.test.ts
+++
@@ -1,9 +0,0 @@
-// import { asyncString } from "./async";
-
-// describe("async module", () => {
-//     test('async data', () => {
-//         asyncString("result!").then(data => {
-//             expect(data).toBe('result');
-//         });
-//     });
-// });
--- example-jest/src/async.ts
+++
@@ -1,5 +0,0 @@
-// export const asyncString = (msg: string) : Promise<string> => {
-//     return new Promise<string>((resolve) => {
-//         setTimeout(resolve, 2000, msg);
-//     });
-// }
